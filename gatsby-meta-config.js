2f4432c307dc04ae62035eb0d843cb221803757a
Sori Rosie Kim <soryonew@gmail.com>
2020-07-19 20:01:17 +0900

    changing profile

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,13 @@
 module.exports = {
   title: `Sori's Develog`,
-  description: `Blog posted about ...`,
+  description: `새로 알게되는 지식과 가끔 떠오르는 단상들을 기록합니다.`,
   author: `[Sori Kim]`,
-  introduction: `주니어프론트앤드개발자: 사회적 가치 실현을 개발을 통해 이루는 것에 관심이 있습니다.`,
+  introduction: `주니어프론트앤드개발자 🍄 사회적 가치 실현을 개발을 통해 이루는 것에 관심을 갖고 있습니다. 
+  기술발전의 사각지대를 늘 고민하는, 모두를 위한 개발을 하고 싶습니다.`,
   siteUrl: `https://sori-kim.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `https://github.com/sori-kim`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
